9452f86a633e3ce5985111f6f067e6d9dc170254
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-18 15:28:13 +0300

    Передает моковые данные в компонент карточки предложения и использует их в разметке

--- project/src/components/offer-card/offer-card.jsx
+++ project/src/components/offer-card/offer-card.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
+import {propOffer} from '../props';
 
-function OfferCard() {
+function OfferCard({offer}) {
+  const {
+    images,
+    isFavorite,
+    isPremium,
+    price,
+    rating,
+    title,
+    type,
+  } = offer;
+  const [cardImage] = images;
+  const favoriteActiveClass = isFavorite ? ' place-card__bookmark-button--active' : '';
+  const ratingStyle = {width: `${Math.round(rating) * 20}%`};
+
   return (
     <article className="cities__place-card place-card">
+      {isPremium &&
       <div className="place-card__mark">
         <span>Premium</span>
-      </div>
+      </div>}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place"/>
+          <img className="place-card__image" src={cardImage} width="260" height="200" alt="Place"/>
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className={`place-card__bookmark-button ${favoriteActiveClass} button`} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
@@ -26,17 +41,21 @@ function OfferCard() {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: '80%'}}></span>
+            <span style={ratingStyle}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
 }
+
+OfferCard.propTypes = {
+  offer: propOffer,
+};
 
 export default OfferCard;
