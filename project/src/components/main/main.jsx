922ffdbf24ce7610dc598e4ae3d9904318bf4444
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-19 15:25:50 +0300

    Изменяет компонент OffersList для переиспользования в компоненте Room

--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Logo from '../logo/logo.jsx';
 import OffersList from '../offers-list/offers-list.jsx';
 import {propOffer} from '../props.js';
+import {AppRoute} from '../../const.js';
 
 function Main(props) {
   const [activeCard, setActiveCard] = useState();
@@ -14,16 +16,16 @@ function Main(props) {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <Logo />
+              <Logo isActive="true" />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
+                  <Link className="header__nav-link header__nav-link--profile" to={AppRoute.FAVORITES}>
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="header__nav-item">
                   <a className="header__nav-link" href="#">
@@ -94,7 +96,12 @@ function Main(props) {
                   <li className="places__option" tabIndex="0">Top rated first</li>
                 </ul>
               </form>
-              <OffersList offers={offers} onCardEnter={(id) => setActiveCard(id)} />
+              <div className="cities__places-list places__list tabs__content">
+                <OffersList
+                  offers={offers}
+                  onCardEnter={(id) => setActiveCard(id)}
+                />
+              </div>
             </section>
             <div className="cities__right-section">
               <section className="cities__map map">
--- project/src/components/offers-list/offers-list.jsx
+++ project/src/components/offers-list/offers-list.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import OfferCard from '../offer-card/offer-card';
 import {propOffer} from '../props';
 
 export default function OffersList({offers, onCardEnter}) {
   return (
-    <div className="cities__places-list places__list tabs__content">
+    <Fragment>
       {offers.map((offer) => (
         <OfferCard
           key={offer.id}
           offer={offer}
           onCardEnter={onCardEnter}
         />))}
-    </div>
+    </Fragment>
   );
 }
 
