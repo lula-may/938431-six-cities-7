f0390aa4d8e6ea7554e649736a6804b1743c4209
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-21 23:11:03 +0300

    Оборачивает обработчики формы комментария в useCallback

--- project/src/components/comment-form/comment-form.jsx
+++ project/src/components/comment-form/comment-form.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
 
 import {MAX_RATING, RATINGS} from '../../const';
@@ -6,16 +6,17 @@ import {MAX_RATING, RATINGS} from '../../const';
 export default function CommentForm({onSubmit}) {
   const [rating, setRating] = useState('');
   const [comment, setComment] = useState('');
+  const handleRatingChange = useCallback(({target}) => setRating(target.value), [setRating]);
+  const handleReviewTextChange = useCallback(({target}) => setComment(target.value), [setComment]);
+  const handleFormSubmit = useCallback((evt) => {
+    evt.preventDefault();
+    onSubmit({rating, comment});
+    setRating('');
+    setComment('');
+  }, [onSubmit, comment, rating]);
 
   return (
-    <form className="reviews__form form" action="#" method="post"
-      onSubmit={(evt) => {
-        evt.preventDefault();
-        onSubmit({rating, comment});
-        setRating('');
-        setComment('');
-      }}
-    >
+    <form className="reviews__form form" action="#" method="post" onSubmit={handleFormSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
         {RATINGS.map((value, i) => {
@@ -25,7 +26,7 @@ export default function CommentForm({onSubmit}) {
             <Fragment key={value}>
               <input className="form__rating-input visually-hidden" name="rating" value={currentRating} id={`${currentRating}-stars`} type="radio"
                 checked={isChecked}
-                onChange={({target}) => setRating(target.value)}
+                onChange={handleRatingChange}
               />
               <label htmlFor={`${currentRating}-stars`} className="reviews__rating-label form__rating-label" title={value}>
                 <svg className="form__star-image" width="37" height="33">
@@ -38,7 +39,7 @@ export default function CommentForm({onSubmit}) {
       </div>
       <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"
         value={comment}
-        onChange={({target}) => setComment(target.value)}
+        onChange={handleReviewTextChange}
       />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
