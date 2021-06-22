23d6b07929ce659228a90cf45adf4dd317ac9b2f
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-22 22:11:42 +0300

    Корректирует описание пропсов

--- project/src/components/props.js
+++ project/src/components/props.js
@@ -1,41 +1,41 @@
 import PropTypes from 'prop-types';
 
 const PROP_LOCATION = PropTypes.shape({
-  'latitude': PropTypes.number.isRequired,
-  'longitude': PropTypes.number.isRequired,
-  'zoom': PropTypes.number.isRequired,
-}).isRequired;
+  latitude: PropTypes.number.isRequired,
+  longitude: PropTypes.number.isRequired,
+  zoom: PropTypes.number.isRequired,
+});
 
 const PROP_CITY = PropTypes.shape({
-  'location': PROP_LOCATION,
-  'name': PropTypes.string.isRequired,
+  location: PROP_LOCATION.isRequired,
+  name: PropTypes.string.isRequired,
 });
 
 const PROP_HOST = PropTypes.shape({
-  'avatarUrl': PropTypes.string.isRequired,
-  'id': PropTypes.number.isRequired,
-  'isPro': PropTypes.bool.isRequired,
-  'name': PropTypes.string.isRequired,
+  avatarUrl: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  isPro: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
 });
 
 const PROP_OFFER = PropTypes.shape({
-  'bedrooms': PropTypes.number.isRequired,
-  'city': PROP_CITY,
-  'description': PropTypes.string.isRequired,
-  'goods': PropTypes.arrayOf(PropTypes.string).isRequired,
-  'host': PROP_HOST,
-  'id': PropTypes.number.isRequired,
-  'images': PropTypes.arrayOf(PropTypes.string).isRequired,
-  'isFavorite': PropTypes.bool.isRequired,
-  'isPremium': PropTypes.bool.isRequired,
-  'location': PROP_LOCATION,
-  'maxAdults': PropTypes.number.isRequired,
-  'previewImage': PropTypes.string.isRequired,
-  'price': PropTypes.number.isRequired,
-  'rating': PropTypes.number.isRequired,
-  'title': PropTypes.string.isRequired,
-  'type': PropTypes.string.isRequired,
-}).isRequired;
+  bedrooms: PropTypes.number.isRequired,
+  city: PROP_CITY,
+  description: PropTypes.string.isRequired,
+  goods: PropTypes.arrayOf(PropTypes.string).isRequired,
+  host: PROP_HOST,
+  id: PropTypes.number.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  isFavorite: PropTypes.bool.isRequired,
+  isPremium: PropTypes.bool.isRequired,
+  location: PROP_LOCATION,
+  maxAdults: PropTypes.number.isRequired,
+  previewImage: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+});
 
 const PROP_COMMENT = PropTypes.shape({
   comment: PropTypes.string.isRequired,
@@ -43,6 +43,6 @@ const PROP_COMMENT = PropTypes.shape({
   id: PropTypes.number.isRequired,
   rating: PropTypes.number.isRequired,
   user: PROP_HOST,
-}).isRequired;
+});
 
 export {PROP_CITY, PROP_COMMENT, PROP_HOST, PROP_LOCATION, PROP_OFFER};
