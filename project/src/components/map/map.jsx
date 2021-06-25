deee97780bf8d19bbb0f3d4d444e344c3abdcdf1
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-25 23:46:20 +0300

    Наполняет компонент Map функционалом по отрисовке маркеров на карте

--- project/src/components/map/map.jsx
+++ project/src/components/map/map.jsx
@@ -1,13 +1,42 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import useMap from '../../hooks/useMap';
 import {PROP_CITY, PROP_OFFER} from '../props';
 
-export default function Map({city, activePoint, points}) {
+export default function Map({city, activeOffer, offers}) {
   const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+  const activeId = activeOffer && activeOffer.id;
 
+  const defaultIcon = leaflet.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [30, 40],
+    iconAnchor: [15, 40],
+  });
+
+  const activeIcon = leaflet.icon({
+    iconUrl: 'img/pin-active.svg',
+    iconSize: [30, 40],
+    iconAnchor: [15, 40],
+  });
+
+  useEffect(() => {
+    if (map) {
+      offers.forEach((offer) => {
+        const isActive = offer.id === activeId;
+        const icon = isActive
+          ? activeIcon
+          : defaultIcon;
+        const {latitude: lat, longitude: lng} = offer.location;
+        leaflet
+          .marker({lat, lng}, {icon})
+          .addTo(map);
+      });
+    }
+  }, [activeId, activeIcon, defaultIcon, map, offers]);
 
   return (
     <section
@@ -19,7 +48,7 @@ export default function Map({city, activePoint, points}) {
 }
 
 Map.propTypes = {
-  activePoint: PROP_OFFER.isRequired,
+  activeOffer: PROP_OFFER,
   city: PROP_CITY.isRequired,
-  points: PropTypes.arrayOf(PROP_OFFER),
+  offers: PropTypes.arrayOf(PROP_OFFER),
 };
