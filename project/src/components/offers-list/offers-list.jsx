b086e13012ce4df3f7284d1a27586f2aa615c515
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-27 17:03:38 +0300

    Добавляет логику по выбору proxi-компонента в зависимости от страницы

--- project/src/components/offers-list/offers-list.jsx
+++ project/src/components/offers-list/offers-list.jsx
@@ -1,16 +1,30 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
-import OfferCard from '../offer-card/offer-card';
+import OfferCardDefault from '../offer-card-default/offer-card-default';
+import OfferCardFavorite from '../offer-card-favorite/offer-card-favorite';
 import {PROP_OFFER} from '../props';
+import {CardType} from '../../const';
 
-export default function OffersList({offers, onCardEnter}) {
+const getComponentByCardType = (type) => {
+  switch (type) {
+    case CardType.FAVORITES:
+      return OfferCardFavorite;
+    default:
+      return OfferCardDefault;
+  }
+};
+
+export default function OffersList({cardType, offers, onCardEnter, onCardLeave}) {
+  const Card = getComponentByCardType(cardType);
   return (
     <Fragment>
       {offers.map((offer) => (
-        <OfferCard
+        <Card
+          type={cardType}
           key={offer.id}
           offer={offer}
           onCardEnter={onCardEnter}
+          onCardLeave={onCardLeave}
           onFavoriteButtonClick={() => {}}
         />))}
     </Fragment>
@@ -18,6 +32,8 @@ export default function OffersList({offers, onCardEnter}) {
 }
 
 OffersList.propTypes = {
+  cardType: PropTypes.string.isRequired,
   offers: PropTypes.arrayOf(PROP_OFFER).isRequired,
   onCardEnter: PropTypes.func.isRequired,
+  onCardLeave: PropTypes.func.isRequired,
 };
