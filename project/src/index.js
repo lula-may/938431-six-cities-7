ad9fe445ccea2448af60355fafbf03f9e7f331f4
Yulia Elagina <julia-elagina@yandex.ru>
2021-06-30 16:23:23 +0300

    Инициализирует хранилище в точке входа, добавляет поддержку ReduxDevTools

--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {OFFERS} from './mocks/offers.js';
 import {COMMENTS} from './mocks/comments.js';
 import {CITIES} from './mocks/cities.js';
+import {reducer} from './store/reducer';
 
 const settings = {
   cities: CITIES,
@@ -12,13 +16,20 @@ const settings = {
   offersCount: 250,
 };
 
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      cities={settings.cities}
-      comments={settings.comments}
-      offers={settings.offers}
-      offersCount={settings.offersCount}
-    />
+    <Provider store={store}>
+      <App
+        cities={settings.cities}
+        comments={settings.comments}
+        offers={settings.offers}
+        offersCount={settings.offersCount}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
