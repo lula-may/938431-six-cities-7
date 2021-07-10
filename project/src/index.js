8075b399394698decdc3c73da2eecee23274b4d1
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-10 23:02:52 +0300

    Переносит вызовы запросов к серверу на старте приложения в компонент app

--- project/src/index.js
+++ project/src/index.js
@@ -7,10 +7,8 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import reducer from './store/root-reducer';
 import {createApi} from './services/api';
-import {fetchOfferList} from './store/offers/api-actions';
 import {ActionCreator } from './store/user/actions';
 import {AuthorizationStatus} from './const';
-import {checkAuth} from './store/user/api-actions';
 
 const api = createApi(
   () => store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH)),
@@ -22,10 +20,6 @@ const store = createStore(
     applyMiddleware(thunk.withExtraArgument(api)),
   ),
 );
-
-
-store.dispatch(fetchOfferList());
-store.dispatch(checkAuth());
 
 ReactDOM.render(
   <React.StrictMode>
