d181ea36d537b960dacbe7b64f918bac2b203edb
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-13 14:09:27 +0300

    Добавляет в компонент Предложений неподалеку логику по отображению прелоадера и сообщения об ошибке

--- project/src/components/near-offers/near-offers.jsx
+++ project/src/components/near-offers/near-offers.jsx
@@ -3,13 +3,17 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 import OffersList from '../offers-list/offers-list.jsx';
+import Spinner from '../spinner/spinner.jsx';
 import {CardType} from '../../const.js';
 import {PROP_OFFER} from '../props.js';
-import {getNearOffers} from '../../store/nearby/selectors.js';
-function NearOffers({offers}) {
+import {getNearOffers, getNearOffersLoadingError, getNearOffersLoadingStatus} from '../../store/nearby/selectors.js';
+
+function NearOffers({isError, isLoading, offers}) {
   return (
     <section className="near-places places">
       <h2 className="near-places__title">Other places in the neighbourhood</h2>
+      {isLoading && <Spinner />}
+      {isError && <p style={{textAlign: 'center'}}>We failed to load data. Please, try again later.</p>}
       <div className="near-places__list places__list">
         <OffersList
           cardClassName="near-places__card"
@@ -23,10 +27,14 @@ function NearOffers({offers}) {
 }
 
 NearOffers.propTypes = {
+  isError: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
   offers: PropTypes.arrayOf(PROP_OFFER).isRequired,
 };
 
 const mapStateToProps = (state) => ({
+  isError: getNearOffersLoadingError(state),
+  isLoading: getNearOffersLoadingStatus(state),
   offers: getNearOffers(state),
 });
 
