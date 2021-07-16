6b6131141c8cfee22c6a6ae5bfbefa7029a3ebe7
Yulia Elagina <julia-elagina@yandex.ru>
2021-07-16 21:32:41 +0300

    Рефакторинг компонентов на странице Favorites

--- project/src/components/favorite-list/favorite-list.jsx
+++ project/src/components/favorite-list/favorite-list.jsx
@@ -5,22 +5,16 @@ import FavoriteCity from '../favorite-city/favorite-city';
 import {getUniqueItems} from '../../utils.js';
 import {getFavoriteOffers} from '../../store/favorite/selectors';
 
-const filterOffersByCity = (offers, name) => offers.filter(({city}) => city.name === name);
-
 function FavoriteList() {
   const favoriteOffers = useSelector(getFavoriteOffers);
 
   const cities = getUniqueItems(favoriteOffers.map(({city}) => city.name));
-  const sortedOffers = cities.map((city) => ({
-    city: city,
-    offers: filterOffersByCity(favoriteOffers, city),
-  }));
   return (
     <section className="favorites">
       <h1 className="favorites__title">Saved listing</h1>
       <ul className="favorites__list">
-        {sortedOffers.map(({city, offers}) => (
-          <FavoriteCity key={city} city={city} offers={offers} />
+        {cities.map((city) => (
+          <FavoriteCity key={city} city={city} />
         ))}
       </ul>
     </section>
