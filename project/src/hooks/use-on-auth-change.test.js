1250b05cbb0f789f36c8a8dc7f8509e70bb6db68
Yulia Elagina <julia-elagina@yandex.ru>
2021-08-04 00:17:54 +0300

    Создает черновой вариант теста для хука useOnAuthChange

---
+++ project/src/hooks/use-on-auth-change.test.js
@@ -0,0 +1,22 @@
+import * as Redux from 'react-redux';
+import {renderHook} from '@testing-library/react-hooks';
+import {useOnAuthChange} from './use-on-auth-change';
+import {AuthorizationStatus} from '../const';
+
+
+describe('Hook: useOnAuthChange', () => {
+  it('should dispatch actions on user authorization status has changed', () => {
+    jest.spyOn(Redux, 'useSelector')
+      .mockReturnValueOnce(AuthorizationStatus.NO_AUTH)
+      .mockReturnValueOnce(false)
+      .mockReturnValueOnce(AuthorizationStatus.AUTH)
+      .mockReturnValueOnce(true);
+
+    const dispatch = jest.fn();
+    jest.spyOn(Redux, 'useDispatch').mockReturnValue(dispatch);
+
+
+    renderHook(() => useOnAuthChange());
+    expect(dispatch).toHaveBeenCalledTimes(4);
+  });
+});
